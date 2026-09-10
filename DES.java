818c0fe0580762ba057dcf8bdd13b882c85aaf28
Thanmanan R <thanmananthanmanan5@gmail.com>
2026-09-10 15:48:38 +0530

    Update and rename EX3Bdes.java to DES.java

--- DES.java
+++ DES.java
@@ -3,7 +3,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Scanner;
-public class des{
+public class DES{
 public static void main(String[] args) throws Exception {
 Scanner sc = new Scanner(System.in);
 KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
