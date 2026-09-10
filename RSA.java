3b66c5418e1b7c52374978f91fe15a77440dbd5a
Thanmanan R <thanmananthanmanan5@gmail.com>
2026-09-10 15:49:25 +0530

    Update and rename EX4rsa.java to RSA.java

--- RSA.java
+++ RSA.java
@@ -1,6 +1,6 @@
 import java.math.BigInteger;
 import java.util.Scanner;
-public class rsa {
+public class RSA {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter prime number p: ");
