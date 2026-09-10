5351671a540fe0c1afd77838b1210eb0f1400bbe
Thanmanan R <thanmananthanmanan5@gmail.com>
2026-09-10 15:41:49 +0530

    Rename class ex2a to RowColumnTransform

--- RowColumnTransform.java
+++ RowColumnTransform.java
@@ -1,4 +1,4 @@
-public class ex2a {
+public class RowColumnTransform {
     public static void main(String[] args) {
         String message = "HELLO WORLD";
         message = message.replace(" ", "");
